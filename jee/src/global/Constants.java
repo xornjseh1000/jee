4ae894c467bc9b802c3782fb72afea1a36db257c
hsh <hsh@hb2006>
2016-06-30 17:25:54 +0900

    32

--- jee/src/global/Constants.java
+++ jee/src/global/Constants.java
@@ -7,7 +7,7 @@ package global;
  * @date   :2016. 6. 17.
  * @author :김동혁
  * @file   :Constants.java
- * @story  :
+ * @story  :112
  */
 public class Constants {
 		public final static String SCHOOL_NAME="한빛학원";
